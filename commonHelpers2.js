d02795791965a8a134919418c8814dccc434970a
andrii248 <88237977+andrii248@users.noreply.github.com>
2023-11-07 10:49:27 +0000

    Deploying to gh-pages from @ andrii248/goit-advancedjs-hw-01@9d82bf598ed37bda91f777532999f5f0f21e5c7f 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const i=document.querySelector("iframe"),e=new r(i);e.on("timeupdate",a(l,1e3));const t=JSON.parse(localStorage.getItem("LS_KEY"))??{};console.log(t);e.setCurrentTime(t);function l({seconds:o}){localStorage.setItem("LS_KEY",JSON.stringify(o))}
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as o,l as r}from"./assets/vendor-78be7656.js";const a=document.querySelector("iframe"),e=new o(a);e.on("timeupdate",r(i,1e3));const m=localStorage.getItem("LS_KEY")??0;e.setCurrentTime(m);function i({seconds:t}){localStorage.setItem("LS_KEY",t)}
 //# sourceMappingURL=commonHelpers2.js.map
